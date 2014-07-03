281b34ef3d2d7613201f54df9fe4aa566b5f5e13
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-07-02 20:01:16 -0400

    Add App icon

--- src/aohara/tinkertime/views/Frame.java
+++ src/aohara/tinkertime/views/Frame.java
@@ -1,7 +1,12 @@
 package aohara.tinkertime.views;
 
 import java.awt.BorderLayout;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 
@@ -20,9 +25,27 @@ public class Frame extends JFrame {
 		setSize(600, 600);
 		setJMenuBar(menuBar);
 		
+		// Add App icons
+		ArrayList<Image> imageList = new ArrayList<Image>();
+		imageList.add(loadIcon("icon 128x128.png"));
+		imageList.add(loadIcon("icon 64x64.png"));
+		imageList.add(loadIcon("icon 32x32.png"));
+		imageList.add(loadIcon("icon 16x16.png"));
+		setIconImages(imageList);
+		
 		add(selectorPanel.getComponent(), BorderLayout.CENTER);
 		add(statusBar.getComponent(), BorderLayout.SOUTH);
 		setVisible(true);
+	}
+	
+	private Image loadIcon(String name){
+		try {
+			URL url = getClass().getClassLoader().getResource("icon/app/" + name);
+			return ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	
