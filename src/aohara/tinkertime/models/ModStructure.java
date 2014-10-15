62c1f4943e39e0df91be8b0624cb64030d51dae2
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-10-15 19:07:21 -0400

    Mod Structure Overhaul

--- src/aohara/tinkertime/models/ModStructure.java
+++ src/aohara/tinkertime/models/ModStructure.java
@@ -1,8 +1,18 @@
 package aohara.tinkertime.models;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.io.IOUtils;
+
+import aohara.tinkertime.Config;
+import thirdParty.ZipNode;
 
 /**
  * Model for discovering and reporting the structure of a Mod Zip File.
@@ -16,17 +26,19 @@ import java.util.Set;
 public class ModStructure {
 	
 	public final String readmeText;
-	private final Set<Module> modules;
+	public final ZipNode gameDataNode;
+	private final Set<ZipNode> modules;
 	public final Path zipPath;
 	
-	public ModStructure(Path zipPath, Set<Module> modules, String readmeText){
+	public ModStructure(Path zipPath, ZipNode gameDataNode, Set<ZipNode> modules, String readmeText){
 		this.zipPath = zipPath;
+		this.gameDataNode = gameDataNode;
 		this.modules = modules;
 		this.readmeText = readmeText;
 	}
 	
-	public boolean usesModule(Module module){
-		for (Module m : modules){
+	public boolean usesModule(ZipNode module){
+		for (ZipNode m : modules){
 			if (m.getName().equals(module.getName())){
 				return true;
 			}
@@ -34,7 +46,71 @@ public class ModStructure {
 		return false;
 	}
 	
-	public Set<Module> getModules(){
-		return new HashSet<Module>(modules);
+	public Set<ZipNode> getModules(){
+		return new HashSet<ZipNode>(modules);
+	}
+	
+	// Factory Methods
+	
+	public static ModStructure inspectArchive(Config config, Mod mod) throws IOException {
+		return inspectArchive(mod.getCachedZipPath(config));
+	}
+	
+	public static ModStructure inspectArchive(final Path zipPath) throws IOException {		
+		try(ZipFile zipFile = new ZipFile(zipPath.toFile())){
+			ZipNode root = ZipNode.fromZipFile(zipFile); // Get structure of zip
+			
+			// Find GameData Path within zip
+			ZipNode gameData = getGameDataNode(root);
+			gameData = gameData != null ? gameData : root;
+			
+			// Discover structure
+			return new ModStructure(zipPath, gameData, getModules(gameData), getReadmeText(zipFile));
+		}
+	}
+	
+	public static String getReadmeText(final Config config, final Mod mod){
+		try(ZipFile zipFile = new ZipFile(mod.getCachedZipPath(config).toFile())){
+			return getReadmeText(zipFile);
+		} catch (IOException e) {}
+		return null;
+	}
+	
+	private static String getReadmeText(final ZipFile zipFile){
+		for (ZipEntry entry : new HashSet<ZipEntry>(Collections.list(zipFile.entries()))){
+			if (!entry.isDirectory() && entry.getName().toLowerCase().contains("readme")){
+				try(StringWriter writer = new StringWriter()){
+					IOUtils.copy(zipFile.getInputStream(entry), writer);
+					return writer.toString();
+				} catch (IOException e) {}
+			}
+		}
+		return null;
+	}
+	
+	private static ZipNode getGameDataNode(final ZipNode zipNode){
+		if (zipNode.getName().toLowerCase().equals("gamedata/")){
+			return zipNode;
+		} else {
+			for (ZipNode child : zipNode.getChildren().values()){
+				ZipNode result = getGameDataNode(child);
+				if (result != null){
+					return result;
+				}
+			}
+		}
+		return null;
+	}
+
+	private static Set<ZipNode> getModules(final ZipNode gameDataNode){
+		Set<ZipNode> modules = new HashSet<>();
+		
+		for (ZipNode child : gameDataNode.getChildren().values()){
+			if (child.isDirectory()){
+				modules.add(child);
+			}
+		}
+		
+		return modules;
 	}
 }
