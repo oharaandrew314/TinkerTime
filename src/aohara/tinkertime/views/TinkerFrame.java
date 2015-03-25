b5a438d0b35a5322f65bd59d6eda5250b33141d9
Andrew O'Hara <ohara.andrew314@gmail.com>
2015-03-25 13:03:14 -0400

    #190 remove Nimbus UI in attempt to fix OSX compatability

--- src/aohara/tinkertime/views/TinkerFrame.java
+++ src/aohara/tinkertime/views/TinkerFrame.java
@@ -2,15 +2,9 @@ package aohara.tinkertime.views;
 
 import java.awt.BorderLayout;
 import java.awt.Image;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
-import javax.swing.InputMap;
 import javax.swing.JFrame;
-import javax.swing.KeyStroke;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
-import javax.swing.text.DefaultEditorKit;
 
 import aohara.common.content.ImageManager;
 import aohara.tinkertime.TinkerTime;
@@ -24,25 +18,6 @@ import aohara.tinkertime.TinkerTime;
 public class TinkerFrame extends JFrame {
 	
 	public TinkerFrame() {
-		// Try to use Nimbus Look & Feel
-		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
-		    
-		    // Fix OSX key-bindings
-		    if ( System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0){
-		    	InputMap im = (InputMap) UIManager.get("TextField.focusInputMap");
-			    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.META_DOWN_MASK), DefaultEditorKit.copyAction);
-			    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.META_DOWN_MASK), DefaultEditorKit.pasteAction);
-			    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.META_DOWN_MASK), DefaultEditorKit.cutAction);
-			    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.META_DOWN_MASK), DefaultEditorKit.selectAllAction);
-		    }
-		   
-		} catch (Exception e) {}
 		
 		setTitle(TinkerTime.NAME);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
