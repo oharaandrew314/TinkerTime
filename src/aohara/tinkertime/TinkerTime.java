b60a6f7379ab821ad0a0b4fabba60e90861a8359
Andrew O'Hara <ohara.andrew314@gmail.com>
2015-06-29 00:27:00 -0400

    Advance version to 1.4.5

--- src/aohara/tinkertime/TinkerTime.java
+++ src/aohara/tinkertime/TinkerTime.java
@@ -24,33 +24,33 @@ import com.google.inject.Injector;
 
 /**
  * Main Class for Tinker Time
- * 
+ *
  * @author Andrew O'Hara
  */
 public class TinkerTime {
-	
+
 	public static final String
-		NAME = "Tinker Time",
-		AUTHOR = "oharaandrew314",
-		DOWNLOAD_URL = "https://kerbalstuff.com/mod/243";
-	public static final Version VERSION = Version.valueOf("1.4.4");
+	NAME = "Tinker Time",
+	AUTHOR = "oharaandrew314",
+	DOWNLOAD_URL = "https://kerbalstuff.com/mod/243";
+	public static final Version VERSION = Version.valueOf("1.4.5");
 	public static final String
-		SAFE_NAME = NAME.replace(" ", ""),
-		FULL_NAME = String.format("%s v%s by %s", NAME, VERSION, AUTHOR);
-	
+	SAFE_NAME = NAME.replace(" ", ""),
+	FULL_NAME = String.format("%s v%s by %s", NAME, VERSION, AUTHOR);
+
 	public static void main(String[] args) {
 		Injector injector = Guice.createInjector(new MainModule());
-		
+
 		// Set Listeners
 		ModUpdateCoordinator modUpdateCoordinator = injector.getInstance(ModUpdateCoordinator.class);
 		modUpdateCoordinator.addHandler(injector.getInstance(ModMetaLoader.class));
 		ModSelectorPanelController selectorPanel = injector.getInstance(ModSelectorPanelFactory.class).create(new Dimension(800, 600), 0.35);
 		modUpdateCoordinator.addHandler(selectorPanel);
-		
+
 		ModManager modManager = injector.getInstance(ModManager.class);
 		modManager.reloadMods();
 		modManager.addListener(injector.getInstance(ModListCellRenderer.class));  // TODO See if can be ported to ModUpdateCoordinator
-		
+
 		try {
 			// Check for App update on Startup
 			TinkerConfig config = injector.getInstance(TinkerConfig.class);
@@ -61,7 +61,7 @@ public class TinkerTime {
 					JOptionPane.showMessageDialog(null, e.toString(), "Error Checking for App Updates", JOptionPane.ERROR_MESSAGE);
 				}
 			}
-			
+
 			// Check for Mod Updates on Startup
 			if (config.autoCheckForModUpdates()){
 				modManager.checkForModUpdates();
@@ -69,7 +69,7 @@ public class TinkerTime {
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e.toString(), "Error Checking for Mod Updates", JOptionPane.ERROR_MESSAGE);
 		}
-		
+
 		// Initialize Frame
 		MenuFactory menuFactory = injector.getInstance(MenuFactory.class);
 		JFrame frame = new JFrame(TinkerTime.FULL_NAME);
