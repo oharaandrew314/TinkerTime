33eb9202390e155b3c7f439658157f42247bda70
Andrew O'Hara <ohara.andrew314@gmail.com>
2015-07-02 21:21:23 -0400

    Fix tests for linux

--- test/io/andrewohara/tinkertime/testUtil/ModTestFixtures.java
+++ test/io/andrewohara/tinkertime/testUtil/ModTestFixtures.java
@@ -2,7 +2,6 @@ package io.andrewohara.tinkertime.testUtil;
 
 import io.andrewohara.common.version.Version;
 import io.andrewohara.tinkertime.models.Installation;
-import io.andrewohara.tinkertime.models.Installation.InvalidGameDataPathException;
 import io.andrewohara.tinkertime.models.mod.Mod;
 
 import java.net.MalformedURLException;
@@ -15,78 +14,71 @@ import java.util.Date;
 
 public class ModTestFixtures {
 
-	private final Installation testInstallation;
-
-	public ModTestFixtures() throws URISyntaxException, InvalidGameDataPathException{
-		Path gameDataPath = Paths.get(getClass().getClassLoader().getResource("GameData").toURI());
-		this.testInstallation = new Installation("test", gameDataPath);
-	}
-
 	////////////////////
 	// Curse Fixtures //
 	////////////////////
 
 	public Mod getCurseEngineerMod(){
-		return new MockMod(1, "http://www.curse.com/ksp-mods/kerbal/220285-kerbal-engineer-redux", "Kerbal Engineer Redux", "cybutek", getDate(2014, 4, 12), "0.6.2.4", "0.24.2", testInstallation);
+		return new MockMod(1, "http://www.curse.com/ksp-mods/kerbal/220285-kerbal-engineer-redux", "Kerbal Engineer Redux", "cybutek", getDate(2014, 4, 12), "0.6.2.4", "0.24.2", null);
 	}
 
 	public Mod getKSRadialMounts(){
-		return new MockMod(2, "https://kerbalstuff.com/api/mod/153", "Radial Engine Mounts by PanaTee Parts International", "teejaye85", Calendar.getInstance().getTime(), "0.30", "0.30", testInstallation);
+		return new MockMod(2, "https://kerbalstuff.com/api/mod/153", "Radial Engine Mounts by PanaTee Parts International", "teejaye85", Calendar.getInstance().getTime(), "0.30", "0.30", null);
 	}
 
 	public Mod getCurseEnhancedNavball(){
 		// Not all data included.  Update
-		return new MockMod(3, "http://www.curse.com/ksp-mods/kerbal/220469-enhanced-navball-v1-3", "Enhanced Navball", "author", null, "1.3.0", "", testInstallation);
+		return new MockMod(3, "http://www.curse.com/ksp-mods/kerbal/220469-enhanced-navball-v1-3", "Enhanced Navball", "author", null, "1.3.0", "", null);
 	}
 
 	public Mod getKSTweakableEverything(){
 		// Not all data included.  Update
-		return new MockMod(4, "https://kerbalstuff.com/mod/255", "TweakableEverything", "author", null, "1.0", "1.0", testInstallation);
+		return new MockMod(4, "https://kerbalstuff.com/mod/255", "TweakableEverything", "author", null, "1.0", "1.0", null);
 	}
 
 	public Mod getCurseMechjeb(){
-		return new MockMod(5, "http://www.curse.com-ksp-mods-kerbal-220221-mechjeb",  "MechJeb", "r4m0n", getDate(2014, 4, 6), "2.2.2.1.0", "0.24.2", testInstallation);
+		return new MockMod(5, "http://www.curse.com-ksp-mods-kerbal-220221-mechjeb",  "MechJeb", "r4m0n", getDate(2014, 4, 6), "2.2.2.1.0", "0.24.2", null);
 	}
 
 	public Mod getCurseHotRockets(){
-		return new MockMod(6, "http://www.curse.com/ksp-mods/kerbal/220207-hotrockets-particle-fx-replacement", "HotRockets! Particle FX Replacement", "Nazari1382", getDate(2014, 7, 1), "0.25", "0.24.2", testInstallation);
+		return new MockMod(6, "http://www.curse.com/ksp-mods/kerbal/220207-hotrockets-particle-fx-replacement", "HotRockets! Particle FX Replacement", "Nazari1382", getDate(2014, 7, 1), "0.25", "0.24.2", null);
 	}
 
 	public Mod getKSTimeControl(){
 		// No Mod zip included in test modCache
-		return new MockMod(7, "https://kerbalstuff.com/api/mod/21", "Time Control", "Xaiier", Calendar.getInstance().getTime(), "13.2", "0.24.2", testInstallation);
+		return new MockMod(7, "https://kerbalstuff.com/api/mod/21", "Time Control", "Xaiier", Calendar.getInstance().getTime(), "13.2", "0.24.2", null);
 	}
 
 	public Mod getKSBackgroundProcessing(){
-		return new MockMod(8, "https://kerbalstuff.com/mod/302", "BackgroundProcessing", "jamespicone", Calendar.getInstance().getTime(), "0.4.0.1", "0.9.0", testInstallation);
+		return new MockMod(8, "https://kerbalstuff.com/mod/302", "BackgroundProcessing", "jamespicone", Calendar.getInstance().getTime(), "0.4.0.1", "0.9.0", null);
 	}
 
 	public Mod getGithubKerbalAlarmClock(){
 		// No Mod zip included in test modCache
-		return new MockMod(9, "https://github.com/TriggerAu/KerbalAlarmClock", "KerbalAlarmClock", "TriggerAu", getDate(2014, 12, 19), "3.2.3.0", null, testInstallation);
+		return new MockMod(9, "https://github.com/TriggerAu/KerbalAlarmClock", "KerbalAlarmClock", "TriggerAu", getDate(2014, 12, 19), "3.2.3.0", null, null);
 	}
 
 	public Mod getGithubProceduralFairings(){
 		// No Mod zip included in test modCache
-		return new MockMod(10, "https://github.com/e-dog/ProceduralFairings", "ProceduralFairings", "e-dog", getDate(2014, 11, 17), "3.11", null, testInstallation);
+		return new MockMod(10, "https://github.com/e-dog/ProceduralFairings", "ProceduralFairings", "e-dog", getDate(2014, 11, 17), "3.11", null, null);
 	}
 
 	public Mod getGithubStockFixes(){
 		// No Mod zip included in test modCache
-		return new MockMod(11, "https://github.com/ClawKSP/KSP-Stock-Bug-Fix-Modules", "KSP-Stock-Bug-Fix-Modules", "ClawKSP", getDate(2015,0,7), "0.1.7", null, testInstallation);
+		return new MockMod(11, "https://github.com/ClawKSP/KSP-Stock-Bug-Fix-Modules", "KSP-Stock-Bug-Fix-Modules", "ClawKSP", getDate(2015,0,7), "0.1.7", null, null);
 	}
 
 	public Mod getGithubActiveTextureManagement(){
 		// No Mod zip included in test modCache
-		return new MockMod(12, "https://github.com/rbray89/ActiveTextureManagement", "ActiveTextureManagement", "rbray89", getDate(2014,11,17), "4.3", null, testInstallation);
+		return new MockMod(12, "https://github.com/rbray89/ActiveTextureManagement", "ActiveTextureManagement", "rbray89", getDate(2014,11,17), "4.3", null, null);
 	}
 
 	public Mod getJenkinsModuleManager(){
-		return new MockMod(13, "https://ksp.sarbian.com/jenkins/job/ModuleManager", "ModuleManager", "ksp.sarbian.com", getDate(2015, 1, 23), "2.5.12", null, testInstallation);
+		return new MockMod(13, "https://ksp.sarbian.com/jenkins/job/ModuleManager", "ModuleManager", "ksp.sarbian.com", getDate(2015, 1, 23), "2.5.12", null, null);
 	}
 
 	public Mod getKSRoverWheelSounds(){
-		return new MockMod(14, "https://kerbalstuff.com/mod/224", "Rover Wheel Sounds", "pizzaoverhead", Calendar.getInstance().getTime(), "1.2", "1.0", testInstallation);
+		return new MockMod(14, "https://kerbalstuff.com/mod/224", "Rover Wheel Sounds", "pizzaoverhead", Calendar.getInstance().getTime(), "1.2", "1.0", null);
 	}
 
 	//////////////////
@@ -106,6 +98,17 @@ public class ModTestFixtures {
 		@Override
 		public int getId(){
 			return id;
+		}
+
+		@Override
+		public Path getZipPath(){
+			String path = "TinkerTime/modCache/" + getId() + ".zip";
+			URL url = getClass().getClassLoader().getResource(path);
+			try {
+				return Paths.get(url.toURI());
+			} catch (URISyntaxException e) {
+				throw new RuntimeException(e);
+			}
 		}
 
 		private static URL getUrl(String url){
